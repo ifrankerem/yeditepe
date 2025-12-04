5e0505f419cb1e1e579163d710459dcfe6ad1787
ifrankerem <ifrankerem@users.noreply.github.com>
2025-12-04 22:12:37 +0300

    lab9 added

--- cse114/hw7/IrfanKerem_Arslan_20220201001.c
+++ cse114/hw7/IrfanKerem_Arslan_20220201001.c
@@ -1,4 +1,4 @@
-//İrfan Kerem Arslan 20220201001-072 HW-6
+//İrfan Kerem Arslan 20220201001-072 HW-7
 #include <stdio.h>
 
 int	*first_in_array(int *first, int *last, int n)
@@ -12,7 +12,7 @@ int	*first_in_array(int *first, int *last, int n)
 			return (p);
 		p++;
 	}
-	return (0);
+	return (last);
 }
 
 int	count_in_array_v3(int *first, int *last, int n)
@@ -25,7 +25,7 @@ int	count_in_array_v3(int *first, int *last, int n)
 	while (p != last)
 	{
 		count++;
-		first = p;
+		first = p + 1;
 		p = first_in_array(first, last, n);
 	}
 	return (count);
@@ -36,5 +36,5 @@ int	main(void)
 	int A[11] = {9, 3, 11, 12, 13, 14, 15, 16, 3, 4, 5};
 	// printf("%p\n", &A[2]);
 	// printf("%p\n", first_in_array(A, A + 11, 11));
-	printf("%d\n", count_in_array_v3(A, A + 11, 3));
+	printf("%d\n", count_in_array_v3(A, A + 11, 4));
 }
---
+++ cse114/lab9/IrfanKerem_Arslan_20220201001.c
@@ -0,0 +1,136 @@
+// 20220201001-072 lAB 9
+
+#include <stdio.h>
+
+int	welcome(void)
+{
+	int	choice;
+
+	printf("Welcome to Labwork 8 String Operations! By using this machine you can do:\n");
+	printf("1. Swap Characters\n");
+	printf("2. Trim Unnecessary Parts\n");
+	printf("3. Count Words\n");
+	printf("4. Exit\n");
+	printf("Make your choice: ");
+	scanf("%d", &choice);
+	return (choice);
+}
+
+void	swap_characters(char *str, int size)
+{
+	char	temp;
+
+	int index1, index2;
+	printf("Enter the first index: ");
+	scanf("%d", &index1);
+	printf("Enter the second index: ");
+	scanf("%d", &index2);
+	if (index1 >= 0 && index1 < size && index2 >= 0 && index2 < size)
+	{
+		temp = str[index1];
+		str[index1] = str[index2];
+		str[index2] = temp;
+		printf("%s\n", str);
+	}
+	else
+	{
+		printf("Invalid indexes!\n");
+	}
+}
+
+void	trim_plus(char *str, int size)
+{
+	char	result[500];
+	int		j;
+
+	j = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (str[i] != '+')
+		{
+			result[j] = str[i];
+			j++;
+		}
+	}
+	result[j] = '\0';
+	printf("%s\n", result);
+}
+
+int	count_words(char *str, int size, char key)
+{
+	int	count;
+	int	newWord;
+
+	count = 0;
+	newWord = 1;
+	for (int i = 0; i < size; i++)
+	{
+		if (str[i] == ' ')
+			newWord = 1;
+		else
+		{
+			if (newWord == 1)
+			{
+				if (str[i] == key || str[i] == (key - 32) || str[i] == (key
+						+ 32))
+				{
+					count++;
+				}
+				newWord = 0;
+			}
+		}
+	}
+	return (count);
+}
+
+int	ft_strlen(char *x)
+{
+	int	i;
+
+	i = -1;
+	while (x[++i])
+		;
+	return (i);
+}
+
+int	main(void)
+{
+	int		choice;
+	char	sentence2[] = "pesim+++ist sago+pa kaj+++mer";
+	char	sentence3[] = "kötü	insanları tanıma senesi";
+	char	swapStr[] = "bukelamun gözler sansi salvo kafiyesi";
+	int		size1;
+	int		size2;
+	char	key;
+	int		size3;
+	int		wordCount;
+
+	choice = welcome();
+	while (choice != 4)
+	{
+		if (choice == 1)
+		{
+			size1 = ft_strlen(swapStr);
+			swap_characters(swapStr, size1);
+		}
+		else if (choice == 2)
+		{
+			size2 = ft_strlen(sentence2);
+			trim_plus(sentence2, size2);
+		}
+		else if (choice == 3)
+		{
+			printf(">> Enter the key: ");
+			scanf(" %c", &key);
+			size3 = ft_strlen(sentence3);
+			wordCount = count_words(sentence3, size3, key);
+			printf("There are %d words that start with %c.\n", wordCount, key);
+		}
+		else
+		{
+			printf("Invalid choice! Please try again.\n");
+		}
+		choice = welcome();
+	}
+	return (0);
+}
